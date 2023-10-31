1bc6ce80bff6b9b98fbbc998b16602f93bb55d05
salam <salamatsecondmail@gmail.com>
2023-10-31 21:35:33 +0300

    1.создан класс 2.код системы 3.добавлен в систему

--- Gitfirst/src/main.java
+++ Gitfirst/src/main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("*Система определения ммоль/ или мг/дл*");
+        System.out.println("Введите свой ммоль:");
+        double mmol = new Scanner(System.in).nextDouble();
+        System.out.println("");
+        System.out.println("*Ваш мг/дл:" +  (mmol/ 0.0555));
+        System.out.println("");
+        System.out.println("Введите ваш мг/дл:");
+        double mgdl = new Scanner(System.in).nextDouble();
+        System.out.println("");
+        System.out.println("*Ваш ммоль/л:" + mgdl/ 18.018);
     }
 }
